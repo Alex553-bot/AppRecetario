c088329016ef6ceb4636d5e4d493caf18ad34dc8
Alex Alvarez <alvarez.rojas.alexander.james5@gmail.com>
2021-11-14 18:38:09 -0400

    init

---
+++ Test_main.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Test_main {
+    public Test_main()
+    {
+    }
+
+    @BeforeEach
+    public void setUp()
+    {
+    }
+
+    @AfterEach
+    public void tearDown()
+    {
+    }
+}
